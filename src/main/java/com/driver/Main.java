ee779980db5ab2777896ce2fd4764813883c911c
Vijay <dhruvakumar.gopisetty@gmail.com>
2024-02-02 20:20:01 +0530

    version 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args){
+        Product p=new Product();
+        int s1=p.product(2,3);
+        int s2=p.product(2,3,4);
+        double s3=p.product(2.0,3.0);
+    }
 
+    public static class Product{
+        public int product(int a, int b){
+            return a*b;
+        }
+        public int product(int a,int b,int c){
+            return a*b*c;
+        }
+        public double product(double a,double b){
+            return a*b;
+        }
+    }
 }
